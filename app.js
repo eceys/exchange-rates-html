fc4d32e0521a6c7be36b797b9f10ae4596d16dae
eceys <78822771+eceys@users.noreply.github.com>
2021-07-16 23:59:41 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+const api = "http://api.exchangeratesapi.io/v1/latest?access_key=c76b4efc9281fad4b9d25709f22b3e55"
+
+//var
+let innerHtmlMoney;
+const button = document.querySelector('.button');
+const select = document.querySelectorAll('select');
+const moneyFrom = document.getElementById('moneyFrom');
+const box = document.querySelector('.text');
+
+//event
+button.addEventListener('click',exchange);
+document.addEventListener("DOMContentLoaded",loadPages);
+
+
+//func
+
+async function loadPages(){
+    moneyCatch();
+}
+
+async function moneyCatch(){
+    const fetchUrl = await fetch(api);
+    const data = await fetchUrl.json();    
+    const keys = Object.keys(data.rates);
+    keys.map(moneyExc => {
+        return innerHtmlMoney += `<option value="${moneyExc}">${moneyExc}</option>`;
+    });
+
+    for(let a=0;a<select.length;a++){
+        select[a].innerHTML = innerHtmlMoney;
+    }
+}
+
+async function convert(a, b){
+    
+    const fetchUrl = await fetch(api);
+    const data = await fetchUrl.json(); 
+    const values = Object.values(data.rates);
+    let writeMoney = moneyFrom.value;
+    if(!writeMoney || writeMoney<=0){
+        alert('Please, write money');
+    }
+
+    let result = ( writeMoney / values[a] ) * values[b];
+    box.innerText = result;
+
+}
+
+async function exchange(){
+    
+    let selectFromMoney = select[0].selectedIndex;
+    let selectToMoney = select[1].selectedIndex;
+    convert(selectFromMoney, selectToMoney);
+}
+
